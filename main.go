449f5efd9c8a57c9cb7d38e417e1c09280374de0
Ashit(Ash) Vyas <aashitvyas@gmail.com>
2020-05-22 12:09:45 -0600

    Chang goegg.go to main.go

---
+++ main.go
@@ -0,0 +1,94 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 21.
+
+// Server3 is an "echo" server that displays request parameters.
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"log"
+	"math"
+	"math/rand"
+	"net/http"
+	"sync"
+)
+
+var palette = []color.Color{color.White, color.Black}
+var count int
+var mu sync.Mutex
+
+const (
+	whiteIndex = 0 // first color in palette
+	blackIndex = 1 // next color in palette
+)
+
+func main() {
+	http.HandleFunc("/animation", animation)
+	http.HandleFunc("/health", health)
+	http.HandleFunc("/count", counter)
+	http.HandleFunc("/", url)
+	log.Fatal(http.ListenAndServe("0.0.0.0:8000", nil))
+}
+
+//!+handler
+// handler echoes the HTTP request.
+func animation(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	lissajous(w)
+	count++
+	mu.Unlock()
+}
+
+func url(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "URL Path = %q\n", r.URL.Path)
+	count++
+	mu.Unlock()
+
+}
+
+func health(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "I am Healthy!!")
+	count++
+	mu.Unlock()
+}
+
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "Count %d\n", count)
+	mu.Unlock()
+}
+
+func lissajous(out io.Writer) {
+	const (
+		cycles  = 5     // number of complete x oscillator revolutions
+		res     = 0.001 // angular resolution
+		size    = 100   // image canvas covers [-size..+size]
+		nframes = 64    // number of animation frames
+		delay   = 8     // delay between frames in 10ms units
+	)
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // phase difference
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
+				blackIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim) // NOTE: ignoring encoding errors
+}
